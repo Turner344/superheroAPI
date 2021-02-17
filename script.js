fabd88beba00844df44f61054fc3d9a99f15e447
Turner Jimerson <turnerj344@gmail.com>
2021-02-17 06:48:58 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+const apiKey = 3223562564405840;
+
+const heroInfo = document.getElementById("heroInfo");
+
+const heroName = document.getElementById("heroName");
+
+async function hero() {
+  const idNumber = Math.floor(Math.random() * 732);
+
+  const heroURL = `https://www.superheroapi.com/api.php/${apiKey}/${idNumber}`;
+
+  const firstFetch = await fetch(heroURL);
+  const heroJSON = await firstFetch.json();
+  return heroJSON;
+}
+
+hero().then((data) => {
+  console.log(data);
+
+  let name = data.name;
+  let affil = data.connections["group-affiliation"];
+  let combat = data.powerstats.combat;
+  let dura = data.powerstats.durability;
+  let intel = data.powerstats.intelligence;
+  let power = data.powerstats.power;
+  let speed = data.powerstats.speed;
+  let strength = data.powerstats.strength;
+  let align = data.biography["alignment"];
+  let fullName = data.biography["full-name"];
+  let image = data.image["url"];
+  // console.log(stats);
+
+  heroInfo.innerHTML = `
+
+<img src=${image} class="image">
+<h4>Full Name: ${fullName}</h4>
+<h5>Alignment: ${align.charAt(0).toUpperCase() + align.slice(1)}</h5>
+<h6> Power Stats </h6>
+<p>Combat: ${combat}</p>
+<p>Durability: ${dura}</p>
+<p>Intelligence: ${intel}</p>
+<p>Power: ${power}</p>
+<p>Speed: ${speed}</p>
+<p>Strength: ${strength}</p>
+<h6>All team affiliations</h6>
+<p>${affil}</p>
+`;
+  heroName.innerText = `Hero Details: ${name.toUpperCase()}`;
+});
